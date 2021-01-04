21a21498a03a8a44b982a6e747ceefee4bdeacb2
weihan-Chen <weihan-Chen@qq.com>
2021-01-04 21:37:02 +0800

    add 2 cpp

---
+++ ruboob_tutorials/1.cpp
@@ -0,0 +1,18 @@
+/*
+ * @Author: weihan-Chen
+ * @Github: https://github.com/weihan-Chen
+ * @Date: 2021-01-04 15:39:53
+ * @LastEditors: weihan-Chen
+ * @LastEditTime: 2021-01-04 15:45:55
+ * @Description: 
+ */
+#include <iostream>
+using namespace std;
+
+// main() 是程序开始执行的地方
+
+int main()
+{
+    cout << "Hello World"; // 输出 Hello World
+    return 0;
+}
